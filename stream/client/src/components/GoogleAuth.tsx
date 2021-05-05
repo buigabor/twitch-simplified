79934b597e58c212bbdc83f9321154030c8dc141
Bui Gábor <bui.gabor97@gmail.com>
2021-05-05 19:02:17 +0200

    Add google login button

--- stream/client/src/components/GoogleAuth.tsx
+++ stream/client/src/components/GoogleAuth.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
 declare global {
   interface Window {
@@ -7,18 +7,43 @@ declare global {
 }
 
 export const GoogleAuth = () => {
-  useEffect(()=>{
-    window.gapi.load('client:auth2', ()=>{
-        window.gapi.client.init({
+  // const [auth, setAuth] = useState<null | any>(null);
+  const [isSignedIn, setIsSignedIn] = useState(null);
+  const auth = useRef<any>()
+
+  const renderAuthButton = ()=>{
+    if(isSignedIn === null){
+      return null;
+    } else if(isSignedIn){
+      return (
+        <button>
+          <i className="fab fa-google"></i>
+        </button>
+      );
+    } else {
+      return <div>I am signed out!</div>;
+    }
+  }
+
+  const onAuthChange = ()=>{
+    setIsSignedIn(auth.current.isSignedIn.get());
+  }
+
+  useEffect(() => {
+    window.gapi.load('client:auth2', () => {
+      window.gapi.client
+        .init({
           clientId:
             '793063093865-enoojjfdhog0e1lor0dook6gs678rkvu.apps.googleusercontent.com',
-          scope:'profile'
+          scope: 'email',
+        })
+        .then(() => {
+          auth.current = window.gapi.auth2.getAuthInstance()
+          setIsSignedIn(auth.current?.isSignedIn.get());
+          auth.current.isSignedIn.listen(onAuthChange)
         });
-    })
-  }, [])
-  return (
-    <div>
+    });
+  }, []);
 
-    </div>
-  )
-}
+  return <div>{renderAuthButton()}</div>;
+};
