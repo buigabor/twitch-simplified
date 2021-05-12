044176a1b7f5578483f793c2291a245aa96f720b
Bui Gábor <bui.gabor97@gmail.com>
2021-05-12 09:41:21 +0200

    Replace redux-form to react-final-form, add form styles

--- stream/client/src/components/Streams/StreamCreate.tsx
+++ stream/client/src/components/Streams/StreamCreate.tsx
@@ -1,30 +1,79 @@
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
 import React from 'react';
-import { connect } from 'react-redux';
-import { Field, InjectedFormProps, reduxForm } from 'redux-form';
+import { Field, Form } from 'react-final-form';
+
+const formStyles = css`
+  input {
+    padding: 5px 0;
+    width: 100%;
+    border-radius: 4px;
+    border: 1.5px rgb(156, 156, 156, 0.4) solid;
+  }
+  .form {
+    &__row {
+      margin-top: 15px;
+      display: flex;
+      flex-direction: column;
+      gap: 10px;
+      label {
+        font-weight: 600;
+      }
+    }
+    &__submit-btn {
+      background-color: #5959f1;
+      padding: 10px 20px;
+      border-radius: 6px;
+      font-weight: 600;
+      font-size: 0.95em;
+      border: none;
+      color: #fff;
+      margin-top: 1rem;
+      cursor: pointer;
+    }
+  }
+`;
 
 interface Props {}
 
-export const StreamCreate: React.FC<InjectedFormProps> = (props) => {
-  const { handleSubmit } = props;
+interface FormValues {
+  [key: string]: string;
+}
+
+export const StreamCreate: React.FC = () => {
+  // Redux Form automatically does event.preventDefault()
+  const onSubmit = (formValues: FormValues) => {
+    console.log(formValues);
+  };
 
   const renderInput = (formProps: any) => {
     return (
-      <input
-        {...formProps.input}
-      />
+      <div className="form__row">
+        <label>{formProps.label}</label>
+        <input {...formProps.input} />
+      </div>
     );
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <Field name="title" component={renderInput} />
-      <Field name="description" component={renderInput} />
-    </form>
+    <Form
+      onSubmit={(values) => {
+        console.log(values);
+      }}
+    >
+      {({ handleSubmit }) => (
+        <form css={formStyles} onSubmit={handleSubmit}>
+          <Field name="title" component={renderInput} label="Enter Title" />
+          <Field
+            name="description"
+            component={renderInput}
+            label="Enter Description"
+          />
+          <button className="form__submit-btn">Submit</button>
+        </form>
+      )}
+    </Form>
   );
 };
 
-const SteamCreateForm = reduxForm<{}, Props>({ form: 'steamCreate' })(
-  StreamCreate,
-);
-
-export default connect(null)(SteamCreateForm);
+export default StreamCreate;
