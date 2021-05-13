7a75523d1c8baa1ac3880fc639fd10fd7e4ce9d3
Bui Gábor <bui.gabor97@gmail.com>
2021-05-13 20:48:53 +0200

    Add basic error handling

--- stream/client/src/components/Streams/StreamCreate.tsx
+++ stream/client/src/components/Streams/StreamCreate.tsx
@@ -5,8 +5,8 @@ import { Field, Form } from 'react-final-form';
 
 const formStyles = css`
   input {
-    padding: 5px 0;
-    width: 100%;
+    font-size: 1em;
+    padding: 7px 10px;
     border-radius: 4px;
     border: 1.5px rgb(156, 156, 156, 0.4) solid;
   }
@@ -15,6 +15,7 @@ const formStyles = css`
       margin-top: 15px;
       display: flex;
       flex-direction: column;
+
       gap: 10px;
       label {
         font-weight: 600;
@@ -23,7 +24,7 @@ const formStyles = css`
     &__submit-btn {
       background-color: #5959f1;
       padding: 10px 20px;
-      border-radius: 6px;
+      border-radius: 5px;
       font-weight: 600;
       font-size: 0.95em;
       border: none;
@@ -32,12 +33,25 @@ const formStyles = css`
       cursor: pointer;
     }
   }
-`;
 
-interface Props {}
+  .error{
+    &__wrapper{
+      background-color: rgb(255, 0, 0, 0.11);
+      color: red;
+      padding: .6rem 1rem;
+      border-radius: 4px;
+      border: 1px red solid;
+    }
+  }
+`;
 
 interface FormValues {
   [key: string]: string;
+}
+
+interface ErrorsFormObject {
+  title?: string;
+  description?: string;
 }
 
 export const StreamCreate: React.FC = () => {
@@ -46,19 +60,46 @@ export const StreamCreate: React.FC = () => {
     console.log(formValues);
   };
 
+  const renderError = ({ error, touched }: any) => {
+    if (touched && error) {
+      return (
+        <div className='error__wrapper'>
+          <div className='error__text'>{error}</div>
+        </div>
+      );
+    }
+
+    return null;
+  };
+
   const renderInput = (formProps: any) => {
     return (
       <div className="form__row">
         <label>{formProps.label}</label>
-        <input {...formProps.input} />
+        <input autoComplete="off" {...formProps.input} />
+        {renderError(formProps.meta)}
       </div>
     );
   };
 
   return (
     <Form
-      onSubmit={(values) => {
-        console.log(values);
+      onSubmit={(formValues) => {
+        console.log(formValues);
+      }}
+      validate={(formValues) => {
+        const errors: ErrorsFormObject = {};
+        // If the input is invalid, we need to return an error object with the errors as key:value
+        // otherwise an we return an empty error object
+
+        // The errors object need the same key names as the Field names
+        if (!formValues.title) {
+          errors.title = 'You must enter a title';
+        }
+        if (!formValues.description) {
+          errors.description = 'You must enter a description';
+        }
+        return errors;
       }}
     >
       {({ handleSubmit }) => (
